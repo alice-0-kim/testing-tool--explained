48f25a87ca1fa670002a015bcf0c61121e237258
Alice <aliceykim0828@gmail.com>
2018-02-09 09:29:43 -0800

    Update test.js

--- TestCafe/test.js
+++ TestCafe/test.js
@@ -1,6 +1,7 @@
 /**
  * A sample test.js file. Allowed extension: .js | .ts
  * For more information, visit here: https://github.com/DevExpress/testcafe
+ *
  */
 import { Selector } from 'testcafe'; // first import testcafe selectors
 
@@ -10,8 +11,17 @@ fixture `Getting Started`
 
 
 //then create a test and place your code there
-test('My first test', async t => {
+test('Page title should contain what the user searched for', async t => {
     await t
         .typeText('#edit-keys', 'Alice Kim')
-        .click('#edit-submit');
+        .click('#edit-submit')
+        .expect(Selector('.page-title').innerText).eql('Search for Alice Kim'); //catches an error when expect fails
+});
+
+//page refreshes and goes back to the starting point specified above using the fixture
+test('Should be able to allow the user to search again', async t => {
+    await t
+        .typeText('#edit-keys', 'alice kim')
+        .click('#edit-submit')
+        .expect(Selector('.page-title').innerText).eql('Search for alice kim');
 });
