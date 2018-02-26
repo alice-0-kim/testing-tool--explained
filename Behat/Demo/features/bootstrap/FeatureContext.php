0a8dfbb73d8ccc1ccfe9a6fda049a3c4edd9dd33
Alice <aliceykim0828@gmail.com>
2018-02-26 12:22:52 -0800

    Update FeatureContext.php

--- Behat/Demo/features/bootstrap/FeatureContext.php
+++ Behat/Demo/features/bootstrap/FeatureContext.php
@@ -1,6 +1,8 @@
 # features/bootstrap/FeatureContext.php
 <?php
-
+/**
+ * @Note: This is for demo.feature
+ */
 use Behat\Behat\Context\Context,
     Behat\Behat\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode,
@@ -36,9 +38,97 @@ class FeatureContext implements Context
     public function iShouldGet(PyStringNode $string)
     {
         if ((string) $string !== $this->output) {
-            throw new Exception(
-                "Actual output is:\n" . $this->output
-            );
+            throw new Exception("Actual output is:\n" . $this->output);
         }
+    }
+}
+
+<?php
+/**
+ * @Note: This is for irenebae.feature
+ */
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\TranslatableContext;
+use Behat\Mink\Element\Element;
+
+use Behat\Gherkin\Node\TableNode;
+
+class FeatureContext extends \Drupal\DrupalExtension\Context\DrupalContext
+{
+    /**
+     * @Given I am at :arg1
+     *
+     * @override
+     */
+    public function iAmAt($arg1)
+    {
+        echo "I want to be at.. " . $arg1;
+        $this->getSession()->visit('http://drupal-8-4-4-clone.dd:8083/' . $arg1);
+        $url = $this->getSession()->getCurrentUrl();
+        $parsed_url = parse_url($url);
+        
+		// echo implode(" ", array_keys($parsed_url));
+		
+        $path = trim($parsed_url['path'], '/');
+        
+        echo "\nNow I am at.. " . $path;
+        
+        return $path;
+    }
+
+    /**
+     * @Then I should see :arg1
+     *
+     * @override
+     */
+    public function iShouldSee($arg1)
+    {
+        echo "I should see.. " . $arg1 . "\n";
+        
+        $this->iAmAt("node/3");
+        $css_selector = ".field--name-title";
+        
+        $element = $this->getSession()->getPage()->find("css", $css_selector);
+        
+        if (empty($element)) {
+            throw new \Exception(sprintf("The page '%s' does not contain the css selector '%s'", $this->getSession()->getCurrentUrl(), $css_selector));
+        }
+        
+        $text = $element->getText();
+        echo "\nNow I see.. " . $text;
+        
+        if (strpos($text, $arg1) !== false) {
+        	echo "\nIt matches!!!";
+        } else {
+        	throw new \Exception(sprintf("The text '%s' does not contain the text '%s' in the page '%s'", $text, $arg1, $this->getSession()->getCurrentUrl()));
+        }
+    }
+
+    /**
+     * @Then I should not see :arg1
+     *
+     * @override
+     */
+    public function iShouldNotSee($arg1)
+    {
+        echo "I should not see.. " . $arg1;
+        
+        $this->iAmAt("node/3");
+        $css_selector = ".field--name-title";
+        
+        $element = $this->getSession()->getPage()->find("css", $css_selector);
+        
+        if (empty($element)) {
+            throw new \Exception(sprintf("The page '%s' does not contain the css selector '%s'", $this->getSession()->getCurrentUrl(), $css_selector));
+        }
+        
+        $text = $element->getText();
+        echo "\nNow I see.. " . $text;
+        
+        if (strpos($text, $arg1) === false) {
+        	echo "\nIt is no where to be seen!!!";
+        } else {
+        	throw new \Exception(sprintf("The text '%s' does not contain the text '%s' in the page '%s'", $text, $arg1, $this->getSession()->getCurrentUrl()));
+        }        
     }
 }
